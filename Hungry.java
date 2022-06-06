2a671bc24e34fc3bf7016179cdda0ae98eb46da9
Danny Tal'or <dannytalor96@gmail.com>
2022-06-07 02:37:31 +0300

    Implement State DP

---
+++ Hungry.java
@@ -0,0 +1,10 @@
+
+public class Hungry implements HungerState {
+
+	@Override
+	public void doAction(Swimmable objSwim) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Satiated.java
@@ -0,0 +1,10 @@
+
+public class Satiated implements HungerState {
+
+	@Override
+	public void doAction(Swimmable objSwim) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
